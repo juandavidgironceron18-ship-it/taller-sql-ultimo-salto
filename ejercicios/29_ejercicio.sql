f3b9be889e49e7ca8790194d033e43ec55065885
juandavidgironceron18-ship-it <juandavidgironceron18@gmail.com>
2026-02-26 21:23:36 -0500

    Refactor SQL query for calculating coupon revenue percentage

--- ejercicios/29_ejercicio.sql
+++ ejercicios/29_ejercicio.sql
@@ -7,13 +7,7 @@
 -- Luego, ese valor se divide entre el ingreso total de todas las ventas
 -- y se multiplica por 100 para obtener el porcentaje.
 SELECT
-    (
-        SUM(
-            CASE
-                WHEN cupon_usado = 'ULTIMO_SUSPIRO' THEN total_venta
-                ELSE 0
-            END
-        ) * 100.0
-        / SUM(total_venta)
-    ) AS porcentaje_con_cupon
+    (SUM(CASE WHEN cupon_usado = 'ULTIMO_SUSPIRO' THEN total_venta ELSE 0 END) * 100.0
+     / SUM(total_venta)) AS porcentaje_con_cupon
 FROM ventas;
+
